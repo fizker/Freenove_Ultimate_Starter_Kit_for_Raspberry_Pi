1cdd6e39546c37fb04edb77cc631f478bd9790e6
Benjamin Horsleben <benjamin@fizkerinc.dk>
2025-05-06 09:34:34 +0200

    Bumped swift-wiringpi to allow compiling on macOS again

--- Code/Swift_Code/Package.swift
+++ Code/Swift_Code/Package.swift
@@ -17,6 +17,7 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.1.2"),
 		.package(url: "https://github.com/fizker/swift-gpio.git", .upToNextMinor(from: "0.3.0")),
+		.package(url: "https://github.com/fizker/swift-wiringpi.git", .upToNextMinor(from: "0.1.2")),
 	],
 	targets: [
 		.executableTarget(
